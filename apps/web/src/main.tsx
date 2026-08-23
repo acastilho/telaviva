a3651d6b5d362868f504cada7165bf49f3f44fc4
acastilho <andxuxa@gmail.com>
2026-08-23 15:18:40 -0300

    feat: load product experience styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,6 +4,7 @@ import { App } from './App'
 import './styles.css'
 import './brand.css'
 import './official-brand.css'
+import './product.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
