68b32c03a04c75017017f01ee8a247b365accd26
acastilho <andxuxa@gmail.com>
2026-08-23 17:04:32 -0300

    fix: use correct vite base across homologation hosts

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,6 +2,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/telaviva/',
+  base: process.env.GITHUB_ACTIONS ? '/telaviva/' : '/',
   plugins: [react()],
 })
